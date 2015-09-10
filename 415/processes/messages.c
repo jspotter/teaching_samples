45304ddce86a79f2a2ebcbf56d34d01ebdc2d622
Jonathan Potter <potter.jonathan.s@gmail.com>
2015-09-09 17:24:39 -0700

    messages

--- 415/processes/messages.c
+++ 415/processes/messages.c
@@ -3,14 +3,22 @@
 #include <unistd.h>
 #include <linux/ipc.h>
 #include <linux/msg.h>
+#include <errno.h>
 
-#define MESSAGE_BUFFER_SIZE 1000
+#define MESSAGE_BUFFER_SIZE 100
+#define MESSAGE_TYPE 1
+
+struct mymsgbuf {
+	long mtype;
+	int request;
+	char content[MESSAGE_BUFFER_SIZE];
+};
 
 int open_queue(key_t keyval)
 {
 	int qid;
 
-	if ((qid = msgget( keyval, IPC_CREAT | IPC_EXCL )) == -1)
+	if ((qid = msgget(keyval, IPC_CREAT | 0660)) == -1)
 	{
 		return -1;
 	}
@@ -22,35 +30,37 @@ int main(int argc, char **argv)
 {
 	int qid;
 	key_t msgkey;
+	struct mymsgbuf msg;
 
 	// Generate our IPC key value
-	msgkey = ftok(".", 'm');
-
+	msgkey = ftok(".", 'b');
+		
 	// Open/create the queue
 	if ((qid = open_queue(msgkey)) == -1) {
-		fprintf(stderr, "Queue already open with key %d\n", msgkey);
+		fprintf(stderr, "Unable to open queue: %d.\n", errno);
 		exit(1);
 	}
 
 	pid_t pid = fork();
 	if (pid == 0) {
-		char message[MESSAGE_BUFFER_SIZE];
-		sprintf(message, "%s", "Hello from the child process!\n");
-		if ((msgsnd(qid, message, MESSAGE_BUFFER_SIZE, 0)) == -1) {
-			fprintf(stderr, "Unable to send message.\n");
+		msg.mtype = MESSAGE_TYPE;
+		msg.request = 1;
+		sprintf(msg.content, "%s", "Hello from the child process!\n");
+
+		if ((msgsnd(qid, &msg, sizeof(struct mymsgbuf) - sizeof(long), 0)) == -1) {
+			fprintf(stderr, "Unable to send message: %d.\n", errno);
 			exit(1);
 		}
 
 		exit(0);
 		
 	} else {
-		char message[MESSAGE_BUFFER_SIZE];
-		if ((msgrcv(qid, message, MESSAGE_BUFFER_SIZE, 0, 0)) == -1) {
-			fprintf(stderr, "Unable to receive message.\n");
+		if ((msgrcv(qid, &msg, sizeof(struct mymsgbuf) - sizeof(long), MESSAGE_TYPE, 0)) == -1) {
+			fprintf(stderr, "Unable to receive message: %d.\n", errno);
 			exit(1);
 		}
 
-		printf("%s", message);
+		printf("%s", msg.content);
 	}
 
 	exit(0);
