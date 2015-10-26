928829608ad06877a9cb77fb5e22459b32b54d15
Jonathan Potter <potter.jonathan.s@gmail.com>
2015-10-26 12:02:42 -0700

    trees

---
+++ 340/10/main.cpp
@@ -0,0 +1,46 @@
+#include "tree.h"
+#include <assert.h>
+using namespace std;
+
+int main()
+{
+	Tree t1, t2;
+	
+	t1.push(6);
+	t1.push(5);
+	t1.push(7);
+
+	t2 = t1;
+	t2.push(26);
+
+	Tree t3(t2);
+	t3.push(0);
+
+	Node* n;
+	n = t1.find(5);
+	assert(n != NULL);
+	n = t2.find(5);
+	assert(n != NULL);
+	n = t3.find(5);
+	assert(n != NULL);
+
+	n = t1.find(26);
+	assert(n == NULL);
+	n = t2.find(26);
+	assert(n != NULL);
+	n = t3.find(26);
+	assert(n != NULL);
+
+	n = t1.find(0);
+	assert(n == NULL);
+	n = t2.find(0);
+	assert(n == NULL);
+	n = t3.find(0);
+	assert(n != NULL);
+
+	t1.print();
+	t2.print();
+	t3.print();
+
+	return 0;
+}
---
+++ 340/10/tree.h
@@ -0,0 +1,40 @@
+#ifndef _TREE_H
+#define _TREE_H
+
+#include <cstdlib>
+
+struct Node
+{
+	// Structs can have constructors too!
+	Node() : left(NULL), right(NULL) {}
+
+	Node* left;
+	Node* right;
+	int value;
+};
+
+class Tree
+{
+public:
+	Tree();
+	Tree(const Tree& other);
+	~Tree();
+	Tree& operator=(const Tree& other);
+
+	void push(int value);
+	Node* find(int value) const;
+	void print() const;
+
+private:
+	Node* start;
+
+	void copyOther(const Tree& other);
+	void clear();
+	Node* findFrom(int value, Node* start) const;
+	void printFrom(Node* start, int numSpaces) const;
+	void copyFrom(Node* start);
+	void clearFrom(Node* start);
+};
+
+#endif
+
